68f0e5ff4cf0e6220e818468b5ecb40767eb204c
impaler <impaler777@gmail.com>
2016-09-18 22:19:06 +1000

    (feature) add delete option for rsync destination deploy.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,5 +3,5 @@
 if  [[ "$TRAVIS_BRANCH" == "dev" ]] || [[ "$TRAVIS_BRANCH" == "master" ]] ;
 then
   rsync -e "ssh -oStrictHostKeyChecking=no -i $TRAVIS_BUILD_DIR/rrsync_haxeflixel \
-  -p $DEPLOY_PORT" -avr $TRAVIS_BUILD_DIR/out $DEPLOY_USER@$DEPLOY_HOST:$TRAVIS_BRANCH
+  -p $DEPLOY_PORT" --delete -avr $TRAVIS_BUILD_DIR/out $DEPLOY_USER@$DEPLOY_HOST:$TRAVIS_BRANCH
 fi
