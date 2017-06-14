8848559320ed010f0caf2d4142430d4427888af8
Chris <impaler@users.noreply.github.com>
2017-06-14 11:32:54 +1000

    (chore) remove unused deploy script.
    Now using the node bin/deploy.js

--- upload.sh
+++
@@ -1,3 +0,0 @@
-# #!/bin/sh
-
-rsync -e "ssh -i $HOME/.ssh/rrsync_haxeflixel -p 776" -av -r --delete-after --quiet /home/omni/projects/haxeflixel/haxeflixel.com/bin rrsync_haxeflixel@107.170.39.41:travis-test
