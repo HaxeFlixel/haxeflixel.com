a228c51a4553657e3679feea7e35bfc480a9d8b0
Christopher Decoster <impaler777@gmail.com>
2018-03-09 12:27:08 +1100

    (fix) update deployment script to work locally and updated enc for new host.

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,21 +1,35 @@
 const execSync = require('child_process').execSync
-const TRAVIS_BRANCH = process.env['TRAVIS_BRANCH'] || 'unknown branch'
 
-if (/dev|master|deploy-test/.test(TRAVIS_BRANCH)) {
-  try {
+// All the required env vars
+const DEPLOY_HOST = process.env['DEPLOY_HOST']
+const DEPLOY_PORT = process.env['DEPLOY_PORT']
+const DEPLOY_USER = process.env['DEPLOY_USER']
+const execOptions = process.env['DEBUG_EXEC'] ? { stdio: [0, 1, 2] } : {}
+var DEPLOY_KEY = process.env['DEPLOY_KEY']
+var SOURCE_DIR = process.env['SOURCE_DIR']
+var GIT_BRANCH = process.env['GIT_BRANCH']
+
+const isTravis = 'TRAVIS' in process.env && 'CI' in process.env
+if (isTravis) {
     const TRAVIS_BUILD_DIR = process.env['TRAVIS_BUILD_DIR']
-    const DEPLOY_HOST = process.env['DEPLOY_HOST']
-    const DEPLOY_PORT = process.env['DEPLOY_PORT']
-    const DEPLOY_USER = process.env['DEPLOY_USER']
-    const rsyncCommand = `rsync -e "ssh -oStrictHostKeyChecking=no -i ${TRAVIS_BUILD_DIR}/rrsync_haxeflixel -p ${DEPLOY_PORT}" --delete -avr ${TRAVIS_BUILD_DIR}/out ${DEPLOY_USER}@${DEPLOY_HOST}:${TRAVIS_BRANCH}`
+    DEPLOY_KEY = `${TRAVIS_BUILD_DIR}/bin/deploy`
+    SOURCE_DIR = `${TRAVIS_BUILD_DIR}/out`
+    GIT_BRANCH = process.env['TRAVIS_BRANCH']
+}
 
-    console.log(`Deploying to haxeflixel.com from branch ${TRAVIS_BRANCH}`)
-    var command = execSync(rsyncCommand)
-    console.log(`rsync command completed with ${command}`)
-  } catch (error) {
-    console.error('error with the rsync command')
-    console.error(error)
-  }
+if (/dev|master|feature/.test(GIT_BRANCH)) {
+    try {
+        const rsyncCommand = `rsync -e "ssh -oStrictHostKeyChecking=no -i ${DEPLOY_KEY} -p ${DEPLOY_PORT}" ` +
+            `--delete -avr ${SOURCE_DIR} ${DEPLOY_USER}@${DEPLOY_HOST}:${GIT_BRANCH}`
+        console.log(`Deploying to haxeflixel.com from branch ${GIT_BRANCH}`)
+
+        execSync(rsyncCommand, execOptions)
+        console.log(`rsync command completed`)
+
+    } catch (error) {
+        console.error('error with the rsync command')
+        console.error(error)
+    }
 } else {
-  console.log(`Not deploying ${TRAVIS_BRANCH}`)
+    console.error(`Cannot deploy from ${GIT_BRANCH} branch, only dev|master|feature is supported`)
 }
