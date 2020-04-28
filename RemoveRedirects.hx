d94c244bf91b8c6e54634eac86b61f3ffc3a856e
Jens Fischer <jensfischer95@gmail.com>
2020-04-28 10:15:02 +0200

    Revert "Let's see if we still need to remove redirects"
    This reverts commit 7f34594bb4f1ba76563c4ff6c57e1dd7e2dcbf09.

---
+++ RemoveRedirects.hx
@@ -0,0 +1,26 @@
+import haxe.io.Path;
+import sys.io.File;
+import sys.FileSystem;
+
+using StringTools;
+
+class RemoveRedirects {
+	static function main() {
+		function loop(dir) {
+			var files = FileSystem.readDirectory(dir);
+			for (file in files) {
+				var path = Path.join([dir, file]);
+				if (FileSystem.isDirectory(path)) {
+					loop(path);
+				}
+				if (Path.extension(file) != "html") {
+					continue;
+				}
+				if (File.getContent(path).contains("This page has moved. You will be automatically redirected to its new location.")) {
+					FileSystem.deleteFile(path);
+				}
+			}
+		}
+		loop("out");
+	}
+}
