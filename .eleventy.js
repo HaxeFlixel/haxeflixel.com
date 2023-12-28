e1022052b2f686cb3b154c75d7db2959095a2605
Cameron <cameron.taylor.ninja@gmail.com>
2023-12-28 02:16:08 -0500

    on-site todo list

--- .eleventy.js
+++ .eleventy.js
@@ -1,27 +1,37 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const { EleventyRenderPlugin } = require('@11ty/eleventy');
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
 const PostCSSPlugin = require("eleventy-plugin-postcss");
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+const markdownIt = require("markdown-it");
+const markdownItTasklist = require("markdown-it-task-lists");
 
-module.exports = function(eleventyConfig) {
-    eleventyConfig.addPlugin(syntaxHighlight);
-    eleventyConfig.addPlugin(EleventyRenderPlugin);
-    eleventyConfig.addPlugin(PostCSSPlugin);
-    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+module.exports = function (eleventyConfig) {
+	eleventyConfig.addPlugin(syntaxHighlight);
+	eleventyConfig.addPlugin(EleventyRenderPlugin);
+	eleventyConfig.addPlugin(PostCSSPlugin);
+	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+
+	// eleventyConfig.addPassthroughCopy({"src/files/images": "images"});
+	eleventyConfig.addPassthroughCopy({ "./public/": "/" });
+	eleventyConfig.addWatchTarget("./out/css/bundle.css");
+
+	eleventyConfig.addLayoutAlias("blog-post", "layouts/blog/post.html");
+
+	let options = {
+		html: true,
+		breaks: true,
+		linkify: true,
+	};
+
+	eleventyConfig.setLibrary("md", markdownIt(options));
+    eleventyConfig.amendLibrary("md", mdLib => mdLib.use(markdownItTasklist));
 
 
-    // eleventyConfig.addPassthroughCopy({"src/files/images": "images"});
-    eleventyConfig.addPassthroughCopy({"./public/": "/"});
-    eleventyConfig.addWatchTarget("./out/css/bundle.css");
-
-
-    eleventyConfig.addLayoutAlias('blog-post', 'layouts/blog/post.html');
-
-    return {
-        dir: {
-            input: "content",
-            output: "out",
-            includes: "../_includes"
-        }
-    };
-}
+	return {
+		dir: {
+			input: "content",
+			output: "out",
+			includes: "../_includes",
+		},
+	};
+};
---
+++ content/todo.md
@@ -0,0 +1,5 @@
+---
+layout: layouts/todo.html
+---
+- [ ] Site seach functionality with fuse.js
+    - [ ] test with blogs
