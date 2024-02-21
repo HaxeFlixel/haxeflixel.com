f30076cdb4414b62b0bde21187c1c068c80a003a
Cameron <cameron.taylor.ninja@gmail.com>
2024-02-21 15:51:36 -0500

    showcase images in progress and standard code style

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,36 @@
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
-
+const Image = require("@11ty/eleventy-img");
+const Sharp = require('sharp');
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(EleventyRenderPlugin);
 
     // used to set the current active dropdown list item to active, depending on current page
     eleventyConfig.addShortcode("dropdownActive", function(dropdownItem) {
-      
       let isActive = this.page.url.includes(`/${dropdownItem}/`);
       return isActive ? "active" : "";
     });
+
+    eleventyConfig.addShortcode("image", async function(src, alt, width, height) {
+      let metadata = await Image(src, { 
+        formats: ["webp", "png"],
+        widths: [width],
+        outputDir: "out/img"
+      });
+  
+      let imageAttributes = {
+        alt,
+        width: width,
+        height: 100,
+        loading: "lazy",
+        decoding: "async",
+      };
+  
+      // You bet we throw an error on a missing alt (alt="" works okay)
+      return Image.generateHTML(metadata, imageAttributes);
+    });
+
+    eleventyConfig.addPassthroughCopy({ "src/files/images": "images" });
   
     // Return your Object options:
     return {
@@ -20,3 +41,9 @@ module.exports = function(eleventyConfig) {
       }
     }
   };
+
+async function resizeImage(src, width, height, mode) {
+    return await Sharp(src)
+        .resize({ width: width, height: height, fit: mode })
+        .toBuffer();
+}
