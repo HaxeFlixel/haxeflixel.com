0ee3a1b9e0d2957aa26eb8bbcbac44c0ad337e17
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2025-01-29 12:40:41 -0500

    run `npm run fix` to auto-format all the crap and add `.cjs` files to `.prettierignore` so it doesn't conflict with standard js formatting

--- scripts/install-flixel-docs-repo.cjs
+++ scripts/install-flixel-docs-repo.cjs
@@ -1,46 +1,46 @@
-const shell = require("shelljs");
-const { exec } = require("child_process");
-const { paths } = require("./install-docs-config.json");
+const shell = require('shelljs')
+const { exec } = require('child_process')
+const { paths } = require('./install-docs-config.json')
 
-if (!shell.which("git")) {
-	shell.echo("Sorry, this script requires git");
-	shell.exit(1);
+if (!shell.which('git')) {
+  shell.echo('Sorry, this script requires git')
+  shell.exit(1)
 }
 
-console.log("Installing flixel-docs to the following paths:");
-console.log(paths.join("\n"));
+console.log('Installing flixel-docs to the following paths:')
+console.log(paths.join('\n'))
 
-const cwd = shell.pwd().stdout;
-console.log("Current folder: " + cwd);
+const cwd = shell.pwd().stdout
+console.log('Current folder: ' + cwd)
 
 for (const path of paths) {
-	installDocsToDir(cwd + path);
+  installDocsToDir(cwd + path)
 }
 
-async function installDocsToDir(path) {
-	if (shell.test("-e", path + "/.git")) {
-		console.log("Updating flixel-docs in " + path + "...");
-		shell.cd(path);
-		exec("git fetch --all", (err, stdout, _) => {
-			if (err) {
-				console.error(err);
-				return;
-			}
+async function installDocsToDir (path) {
+  if (shell.test('-e', path + '/.git')) {
+    console.log('Updating flixel-docs in ' + path + '...')
+    shell.cd(path)
+    exec('git fetch --all', (err, stdout, _) => {
+      if (err) {
+        console.error(err)
+        return
+      }
 
-			exec("git pull", (pullerr, pullstdout, _) => {
-				if (pullerr) {
-					console.error(pullerr);
-					return;
-				}
-				console.log(pullstdout.toString());
-			});
+      exec('git pull', (pullerr, pullstdout, _) => {
+        if (pullerr) {
+          console.error(pullerr)
+          return
+        }
+        console.log(pullstdout.toString())
+      })
 
-			shell.cd(cwd);
-		});
-	} else {
-		console.log("Cloning flixel-docs to " + path + "...");
-		exec(
-			"git clone --depth 1 --no-single-branch https://github.com/haxeflixel/flixel-docs " + path,
-		);
-	}
+      shell.cd(cwd)
+    })
+  } else {
+    console.log('Cloning flixel-docs to ' + path + '...')
+    exec(
+      'git clone --depth 1 --no-single-branch https://github.com/haxeflixel/flixel-docs ' + path
+    )
+  }
 }
